b62a7c0947887974e9f1453890e9a2b77753b309
ajroot5685 <anjeong8577@naver.com>
2023-05-15 00:45:51 +0900

    feat: update and find

--- src/main/java/com/springboot/web/dto/PostsUpdateRequestDto.java
+++ src/main/java/com/springboot/web/dto/PostsUpdateRequestDto.java
@@ -3,8 +3,10 @@ package com.springboot.web.dto;
 import com.springboot.Domain.posts.Posts;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class PostsUpdateRequestDto {
     private String title;
     private String content;
